cf163e8dbd5bee4920a7d5d3a4dff11c49a206c7
ankitdani <ankitdani1997@gmail.com>
2024-02-28 19:06:08 -0800

    Skeleton code for project

---
+++ src/main/java/com/example/Medical/Service/Location/MedicalServiceLocationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Medical.Service.Location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalServiceLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalServiceLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Medical/Service/Location/MedicalServiceLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Medical.Service.Location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalServiceLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
